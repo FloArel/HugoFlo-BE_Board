8353d71e26c0ed4d889a6016cf303fd402aa436d
Hugo L <lefevrehugo@ymail.com>
2020-05-20 12:21:36 +0200

    classes dice et player (player a terminer)

--- src_proj/internal.h
+++ src_proj/internal.h
@@ -5,14 +5,16 @@
 #include <thread>
 #include <unistd.h>
 #include <string.h>
-#include "core_simulation.h"
+//#include "core_simulation.h"
+
+using namespace std;
 
 class Dice{
     private:
         int value;
     public:
         Dice();
-        virtual ~Dice();
+        virtual ~Dice(){};
         int read_val();
         void throw_dice();
 };
@@ -23,12 +25,25 @@ class Player{
     private:
         string name;
         Player* suiv;
+        string Roles[15];
 
 
     public:
         Player();
         virtual ~Player();
-        void enter_name();
+        void enter_name(string nom);
+        string get_name();
+        Player* get_suiv();
+
 
 
 };
+
+class Liste{
+    private:
+        
+    public:
+
+};
+
+#endif
