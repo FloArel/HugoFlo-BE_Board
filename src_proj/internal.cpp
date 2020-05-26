4e7158356c0f316cfbb4426890c6eae949bfc7c8
Hugo L <lefevrehugo@ymail.com>
2020-05-26 18:59:12 +0200

    Ajout roles, players et liste de joueurs

--- src_proj/internal.cpp
+++ src_proj/internal.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 #define TESTDE 0
 #define TESTPLAY 1
+#define TESTLISTE 1
 
 //var globale pour savoir quels roles sont attribues avec masques
 //static int roles_attrib=0;
@@ -13,9 +14,9 @@ using namespace std;
 static int nb_joueurs=0;
 
 //pointeurs joueurs pour parcours de la liste chainee
-static Player* dernier=NULL;
+/*static Player* dernier=NULL;
 static Player* premier=NULL;
-static Player* J_courant=NULL;
+static Player* J_courant=NULL;*/
 
 //constructeur dice
 Dice::Dice(){
@@ -42,24 +43,13 @@ void Dice::throw_dice(){
 //constructeur player
 Player::Player(){
     name="joueur sans nom";
-    int i;
-    for (i = 0; i < 15; i++)
-    {
-        Roles[i]="rien";
-    }
-
-    if (nb_joueurs==0){
-        suiv=NULL;
-        premier=this;
-        dernier=this;
-    }
-    else
-    {
-        suiv=NULL;
-        dernier->suiv=this;
-        dernier=this;
-    }
+    nb_joueurs++;
     
+    
+}
+Player::Player(string nom){
+    name=nom;
+    nb_joueurs++;
 }
 
 //destructeur player
@@ -76,8 +66,90 @@ string Player::get_name(){
     return name;
 }
 
-Player* Player::get_suiv(){
-    return suiv;
+//constructeur roles
+//constructeur: Tous les rôles sont attribués à un faux joueur
+Roles::Roles(){
+    
+}
+
+Player* Roles::getPlayer(int role){
+    switch (role)
+        {
+        case DIEU:
+            return Dieu;
+            break;
+        case HEROS:
+            return Heros;
+            break;
+        case ORACLE:
+            return Oracle;
+            break;
+        case ECUYER:
+            return Ecuyer;
+            break;
+        case PRISONNIER:
+            return Prisonnier;
+            break;
+        case CATIN:
+            return Catin;
+            break;
+        case AUBERGISTE:
+            return Aubergiste;
+            break;
+        case PRINCESSE:
+            return Princesse;
+            break;
+        case DRAGON:
+            return Dragon;
+            break;
+        //voir si on raise une erreur
+        default:
+            return None;
+            break;
+        }
+}
+
+void Roles::setPlayerRole(Player* attrib, int role){
+    switch (role){
+        case DIEU:
+            Dieu=attrib;
+            break;
+        case HEROS:
+            Heros=attrib;
+            break;
+        case ORACLE:
+            Oracle=attrib;
+            break;
+        case ECUYER:
+            Ecuyer=attrib;
+            break;
+        case PRISONNIER:
+            Prisonnier=attrib;
+            break;
+        case CATIN:
+            Catin=attrib;
+            break;
+        case AUBERGISTE:
+            Aubergiste=attrib;
+            break;
+        case PRINCESSE:
+            Princesse=attrib;
+            break;
+        case DRAGON:
+            Dragon=attrib;
+            break;
+        }
+}
+
+
+// surcharge operateur += pour le nom de joueur
+Player& Player::operator+= (const string & nom){
+    if (nom == ""){
+        cout << "Le nom ne peut pas etre vide!\n";
+        exit(0);
+    }
+    enter_name(nom);
+    return (*this);
 }
 
 //TODO: classe rôles avec un attribut pour chaque qui prend un player?
@@ -97,19 +169,35 @@ int main(){
         J1.enter_name("patate");
         Player J2;
         J2.enter_name("radis");
-        int i
-        for (i = 0; i < 15; i++)
-        {
-            
-        }
+        Player J3("fraise");
+        J1+="Plus radis";
         
-        
+
+    #endif
+
+    #if TESTLISTE
+        vector<Player> Joueurs;
+        vector<Player>::iterator courant;
+        Joueurs.push_back(J1);
+        Joueurs.push_back(J2);
+        Joueurs.push_back(J3);
+        Roles Game1;
+        Game1.setPlayerRole(&J3,DRAGON);
+        cout<<"le dragon est "<<Game1.getPlayer(DRAGON)->get_name()<<endl;
+        //cout<<Game1.getPlayer(DIEU)->get_name()<<endl;
+        cout<<"nb joueurs crees "<<nb_joueurs<<endl;
 
 
     #endif
 
-    return 0;
 
+    courant=Joueurs.begin();
+    cout<<courant->get_name()<<endl;
+    courant++;
+    cout<<courant->get_name()<<endl;
+
+
+    return 0;
 
 
 }
--- src_proj/internal.h
+++ src_proj/internal.h
@@ -1,10 +1,23 @@
 #ifndef INTERNAL_H
 #define INTERNAL_H
 
+
+#define DIEU 1
+#define HEROS 2
+#define ORACLE 3
+#define ECUYER 4
+#define PRISONNIER 5
+#define CATIN 6
+#define AUBERGISTE 7
+#define PRINCESSE 8
+#define DRAGON 9
+
 #include <iostream>
 #include <thread>
 #include <unistd.h>
 #include <string.h>
+#include <vector>
+
 //#include "core_simulation.h"
 
 using namespace std;
@@ -24,25 +37,45 @@ class Dice{
 class Player{
     private:
         string name;
-        Player* suiv;
-        string Roles[15];
+        //Player* suiv;
+        //string Roles[15];
 
 
     public:
         Player();
+        Player(string nom);
         virtual ~Player();
         void enter_name(string nom);
         string get_name();
-        Player* get_suiv();
+        Player& operator+=(const string & nom);
+        //Player* get_suiv();
+        
 
 
 
 };
 
-class Liste{
+class Roles{
     private:
+        //TODO attribut tableau de player pour none et prisonnier?
+        Player* None;
+        Player* Dieu;
+        Player* Heros;
+        Player* Oracle;
+        Player* Ecuyer;
+        Player* Prisonnier;
+        Player* Catin;
+        Player* Aubergiste;
+        Player* Princesse;
+        Player* Dragon;
         
     public:
+        
+        Roles();
+        Player* getPlayer(int role);
+
+        void setPlayerRole(Player* attrib, int role);
+        
 
 };
 
